ee0e8e04181a05d3d7f272d9002c4bc462419d9a
Artem L <artuon@gmail.com>
2020-12-11 22:13:00 +0200

    Add Firebase DB for inserting and the store

--- store/index.js
+++ store/index.js
@@ -1,5 +1,5 @@
 import Vuex from 'vuex'
-// import axios from 'axios'
+import axios from 'axios'
 import { searchImages } from 'pixabay-api'
 
 const pixabayKey = '19446257-b0025af71a07915d6889c5664'
@@ -23,47 +23,43 @@ const CreateStore = () => {
     },
     actions: {
       nuxtServerInit(vuexContext, context) {
-        return new Promise((resolve, reject) => {
-          setTimeout(() => {
-            vuexContext.commit('setCards', [
-              {
-                id: '1',
-                word: 'Word 1',
-                annotation: 'Trans Word 1',
-                image: 'https://picsum.photos/600/500?1',
-              },
-              {
-                id: '2',
-                word: 'Word 2',
-                annotation: 'Trans Word 2',
-                image: 'https://picsum.photos/600/500?2',
-              },
-              {
-                id: '3',
-                word: 'Word 3',
-                annotation: 'Trans Word 3',
-                image: 'https://picsum.photos/600/500?3',
-              },
-            ])
-            resolve()
-          }, 1000)
-        })
+        return axios
+          .get('https://my-cards-2021-default-rtdb.firebaseio.com/words.json')
+          .then((res) => {
+            const cards = []
+            for (const key in res.data) {
+              cards.push({ ...res.data[key], id: key })
+            }
+            vuexContext.commit('setCards', cards)
+          })
+          .catch((e) => context.error(e))
       },
       setCards(vuexContext, cards) {
         vuexContext.commit('setCards', cards)
       },
       addCard(vuexContext, card) {
-        card.id = Date.now().toString()
         card.image = ''
         searchImages(pixabayKey, card.word, { per_page: 3 })
           .then((r) => {
             if (r.total > 0) {
               card.image = r.hits[0].largeImageURL
+              axios
+                .post(
+                  'https://my-cards-2021-default-rtdb.firebaseio.com/words.json',
+                  card
+                )
+                .then((result) => {
+                  vuexContext.commit('addCard', {
+                    ...card,
+                    id: result.data.name,
+                  })
+                })
+                // eslint-disable-next-line no-console
+                .catch((e) => console.log(e))
             }
           })
           // eslint-disable-next-line no-console
           .catch((e) => console.error(e))
-        vuexContext.commit('addCard', card)
       },
       saveCard(vuexContext, card) {
         vuexContext.commit('saveCard', card)
