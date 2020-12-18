a3dfa5ef3bf20866bc76d3d901fa98904c8b2e88
Artem L <artuon@gmail.com>
2020-12-18 14:13:38 +0200

    Adding common.scss

--- nuxt.config.js
+++ nuxt.config.js
@@ -17,7 +17,10 @@ export default {
   },
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
-  css: [],
+  css: [
+    // SCSS file in the project
+    '@/assets/common.scss'
+  ],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [],
