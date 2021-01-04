7b35bc341e98669add2d29274b8ecd6dc4c72568
Artem L <artuon@gmail.com>
2021-01-04 22:03:40 +0200

    User Edit Form: Validate "Save" Button

--- components/user/UserForm.vue
+++ components/user/UserForm.vue
@@ -4,7 +4,7 @@
       <span class="headline">Edit User</span>
     </v-card-title>
     <v-card-text>
-      <v-form ref="form" v-model="valid" lazy-validation>
+      <v-form ref="formUser" v-model="valid" lazy-validation>
         <v-text-field
           v-model="firstName"
           :rules="[rules.required]"
@@ -24,7 +24,11 @@
     </v-card-text>
     <v-card-actions>
       <v-spacer></v-spacer>
-      <v-btn color="primary" text :disabled="!valid" @click="submitUser"
+      <v-btn
+        color="primary"
+        text
+        :disabled="submitButtonState"
+        @click="submitUser"
         >Save
       </v-btn>
       <v-btn color="primary" text @click="cancel"> Cancel</v-btn>
@@ -42,8 +46,12 @@ export default {
     },
   },
   data: () => ({
+    input: {
+      firstName: '',
+      lastName: '',
+    },
     loading: false,
-    valid: true,
+    valid: false,
     firstName: '',
     lastName: '',
     email: '',
@@ -51,11 +59,23 @@ export default {
       required: (value) => !!value || 'Required.',
     },
   }),
+  computed: {
+    submitButtonState() {
+      return (
+        (this.input.firstName === this.firstName &&
+          this.input.lastName === this.lastName) ||
+        this.firstName.length === 0 ||
+        this.lastName.length === 0
+      )
+    },
+  },
   mounted() {
     const user = this.$store.getters['auth/user']
     this.firstName = user.firstName
     this.lastName = user.lastName
     this.email = user.email
+    this.input.firstName = user.firstName
+    this.input.lastName = user.lastName
   },
   methods: {
     toggleLoading() {
@@ -65,15 +85,19 @@ export default {
       this.$emit('dialog-reverse')
     },
     submitUser() {
-      this.toggleLoading()
-      const userData = {
-        firstName: this.firstName,
-        lastName: this.lastName,
-      }
-      this.$store.dispatch('auth/setUserData', userData).then(() => {
+      if (this.$refs.formUser.validate()) {
         this.toggleLoading()
-        this.$emit('dialog-reverse')
-      })
+        const userData = {
+          firstName: this.firstName,
+          lastName: this.lastName,
+        }
+        this.$store.dispatch('auth/setUserData', userData).then(() => {
+          this.input.firstName = this.firstName
+          this.input.lastName = this.lastName
+          this.toggleLoading()
+          this.$emit('dialog-reverse')
+        })
+      }
     },
   },
 }
