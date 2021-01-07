ca221c9bd6d27a14f71ee4b1d8c2a2e62b7f8535
Artem L <artuon@gmail.com>
2021-01-07 21:01:59 +0200

    Playing with v-combobox for filter

--- pages/test.vue
+++ pages/test.vue
@@ -1,11 +1,38 @@
 <template>
-  <div>
-    <h1>Test</h1>
-  </div>
+  <v-combobox
+    v-model="select"
+    :items="items"
+    label="I use a scoped slot"
+    multiple
+    chips
+  >
+    <template v-slot:selection="data">
+      <v-chip
+        :key="JSON.stringify(data.item)"
+        v-bind="data.attrs"
+        :input-value="data.selected"
+        :disabled="data.disabled"
+        @click:close="data.parent.selectItem(data.item)"
+      >
+        <v-avatar
+          class="accent white--text"
+          left
+          v-text="data.item.slice(0, 1).toUpperCase()"
+        ></v-avatar>
+        {{ data.item }}
+      </v-chip>
+    </template>
+  </v-combobox>
 </template>
 
 <script>
 export default {
   middleware: ['auth'],
+  data() {
+    return {
+      select: ['Vuetify', 'Programming'],
+      items: ['Programming', 'Design', 'Vue', 'Vuetify'],
+    }
+  },
 }
 </script>
