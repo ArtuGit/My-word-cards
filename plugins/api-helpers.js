b87e3716ca1c32797edf5bb8d46bdaa6a3d62bf0
Artem L <artuon@gmail.com>
2021-01-17 11:29:19 +0200

    Take first 2 words for an Pixabay image query

--- plugins/api-helpers.js
+++ plugins/api-helpers.js
@@ -18,25 +18,28 @@ const fakeRequestPromise = (delay = 1000) => {
 
 async function getPixabayImage(phrase, type = 'comments') {
   try {
-    const data = await searchImages(phrase, { per_page: 200 })
-    let largeImageURL = ''
-    if (data.total > 0) {
-      if (type === 'comments') {
-        let maxComments = 0
-        data.hits.forEach((item) => {
-          if (item.comments >= maxComments) {
-            maxComments = item.comments
-            largeImageURL = item.largeImageURL
-          }
-        })
-      } else if (type === 'random') {
-        const index = Math.floor(Math.random() * data.hits.length)
-        largeImageURL = data.hits[index].largeImageURL
-      } else if (type === 'first') {
-        largeImageURL = data.hits[0].largeImageURL
+    const phraseSearch = phrase.split(' ').slice(0, 2).join(' ') // first words
+    if (phraseSearch) {
+      const data = await searchImages(phraseSearch, { per_page: 200 })
+      let largeImageURL = ''
+      if (data.total > 0) {
+        if (type === 'comments') {
+          let maxComments = 0
+          data.hits.forEach((item) => {
+            if (item.comments >= maxComments) {
+              maxComments = item.comments
+              largeImageURL = item.largeImageURL
+            }
+          })
+        } else if (type === 'random') {
+          const index = Math.floor(Math.random() * data.hits.length)
+          largeImageURL = data.hits[index].largeImageURL
+        } else if (type === 'first') {
+          largeImageURL = data.hits[0].largeImageURL
+        }
       }
-    }
-    return largeImageURL
+      return largeImageURL
+    } else return ''
   } catch (err) {
     // eslint-disable-next-line no-console
     console.error(err)
