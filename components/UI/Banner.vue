16554afb1832f3672836470fce79b67319598862
Artem Laptiev <artuon@gmail.com>
2021-02-13 21:21:11 +0200

    Trying components on the home page

--- components/UI/Banner.vue
+++ components/UI/Banner.vue
@@ -4,20 +4,75 @@
   -->
 
 <template>
-  <div id="hero">
-    <video playsinline autoplay muted loop poster="@/static/images/banner.jpg">
-      <source :src="require('@/static/videos/banner.mp4')" type="video/mp4" />
-    </video>
+  <div>
+    <v-container fluid class="grey lighten-5">
+      <v-row class="mb-6" no-gutters>
+        <v-col
+          v-for="n in 2"
+          :key="n"
+          :lg="cols[n - 1]"
+          :md="6"
+          :sm="cols[n - 1]"
+        >
+          <v-card class="pa-2" outlined tile> col-{{ cols[n - 1] }} </v-card>
+        </v-col>
+      </v-row>
+      <v-row no-gutters>
+        <v-col v-for="n in 3" :key="n" cols="sm">
+          <v-card class="pa-2" outlined tile> col </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+
+    <v-container fluid>
+      <video
+        playsinline
+        autoplay
+        muted
+        loop
+        poster="@/static/images/banner.jpg"
+      >
+        <source :src="require('@/static/videos/banner.mp4')" type="video/mp4" />
+      </video>
+    </v-container>
+
+    <v-container fluid>
+      <v-carousel v-model="model">
+        <v-carousel-item v-for="(color, i) in colors" :key="color">
+          <v-sheet :color="color" height="100%" tile>
+            <v-row class="fill-height" align="center" justify="center">
+              <div class="display-3">Slide {{ i + 1 }}</div>
+            </v-row>
+          </v-sheet>
+        </v-carousel-item>
+      </v-carousel>
+    </v-container>
   </div>
 </template>
 
+<script>
+export default {
+  data() {
+    return {
+      model: 0,
+      colors: ['primary', 'secondary', 'yellow darken-2', 'red', 'orange'],
+    }
+  },
+  computed: {
+    cols() {
+      const { lg, sm } = this.$vuetify.breakpoint
+      return lg ? [3, 9] : sm ? [9, 3] : [6, 6]
+    },
+  },
+}
+</script>
+
 <style>
-video {
-  object-fit: cover;
+#v-banner {
   width: 100vw;
   height: 35rem;
-  position: fixed;
-  top: 0;
-  left: 0;
+}
+video {
+  object-fit: cover;
 }
 </style>
