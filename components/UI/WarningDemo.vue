1e274a9396078079a72c5cc5a9692748e49dcea1
Artem Laptiev <artuon@gmail.com>
2021-02-13 21:49:40 +0200

    Starting WarningDemo.vue

---
+++ components/UI/WarningDemo.vue
@@ -0,0 +1,13 @@
+<!--
+  - Developed by Artu, https://github.com/ArtuGit
+  - Copyleft 2020-2021.
+  -->
+
+<template>
+  <v-snackbar :timeout="-1" :value="true" top color="primary" outlined>
+    Lorem ipsum dolor sit amet consectetur. Lorem ipsum dolor sit amet
+    consectetur. Lorem ipsum dolor sit amet consectetur. Lorem ipsum dolor
+    sit amet consectetur.
+  </v-snackbar>
+</template>
+
--- pages/test.vue
+++ pages/test.vue
@@ -4,7 +4,17 @@
   -->
 
 <template>
-  <div>Test</div>
+  <div>
+    <div>Test</div>
+
+    <v-card height="300" flat>
+      <v-snackbar :timeout="-1" :value="true" top color="primary" outlined>
+        Lorem ipsum dolor sit amet consectetur. Lorem ipsum dolor sit amet
+        consectetur. Lorem ipsum dolor sit amet consectetur. Lorem ipsum dolor
+        sit amet consectetur.
+      </v-snackbar>
+    </v-card>
+  </div>
 </template>
 
 <script>
