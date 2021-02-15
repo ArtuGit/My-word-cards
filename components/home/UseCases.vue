3aa41f109862149ed8197058aacc74aa3969208c
Artem L <artuon@gmail.com>
2021-02-15 21:40:34 +0200

    Developing UseCases.vue

--- components/home/UseCases.vue
+++ components/home/UseCases.vue
@@ -4,26 +4,29 @@
   -->
 
 <template>
-  <v-row>
-    <v-col
-      v-for="caseCurr in cases"
-      :key="caseCurr.title"
-      cols="12"
-      sm="6"
-      md="4"
-      lg="4"
-      xl="4"
-    >
-      <v-card class="mx-auto" max-width="344">
-        <v-img
-          :src="require('@/static/images/tmp/undraw_Analysis_re_w2vd.svg')"
-          :aspect-ratio="undefined"
-        ></v-img>
-        <v-card-title> Top western road trips</v-card-title>
-        <v-card-subtitle> 1,000 miles of wonder</v-card-subtitle>
-      </v-card>
-    </v-col>
-  </v-row>
+  <div>
+    <div class="text-h3 text-center py-12">Use Cases</div>
+    <v-row>
+      <v-col
+        v-for="caseCurr in cases"
+        :key="caseCurr.title"
+        cols="12"
+        sm="6"
+        md="4"
+        lg="4"
+        xl="4"
+      >
+        <v-card class="mx-auto" max-width="344" height="100%">
+          <v-img
+            :src="require('@/static/images/tmp/' + caseCurr.image)"
+            :aspect-ratio="undefined"
+          ></v-img>
+          <v-card-title>{{ caseCurr.title }}</v-card-title>
+          <v-card-subtitle> {{ caseCurr.subTitle }}</v-card-subtitle>
+        </v-card>
+      </v-col>
+    </v-row>
+  </div>
 </template>
 
 <script>
@@ -31,11 +34,20 @@ export default {
   data() {
     return {
       cases: [
-        { title: 'Languages Learning', subTitle: '1,000 miles of wonder' },
-        { title: 'New Terms Learning', subTitle: '2,000 miles of wonder' },
+        {
+          title: 'Languages Learning',
+          subTitle: '1,000 miles of wonder',
+          image: 'undraw_professional_card_otb4.svg',
+        },
+        {
+          title: 'New Terms Learning',
+          subTitle: '2,000 miles of wonder',
+          image: 'undraw_Modern_life_re_8pdp.svg',
+        },
         {
           title: 'Inventories and Collections',
           subTitle: '3,000 miles of wonder',
+          image: 'undraw_preferences_popup_wbfw.svg',
         },
       ],
     }
