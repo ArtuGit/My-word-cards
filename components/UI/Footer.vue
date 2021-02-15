4c14161684513bab583a69274b6db3a237fed73f
Artem Laptiev <artuon@gmail.com>
2021-02-15 15:40:57 +0200

    Developing the footer

--- components/UI/Footer.vue
+++ components/UI/Footer.vue
@@ -6,12 +6,20 @@
 <template>
   <v-footer dark padless>
     <v-container fluid>
-      <v-row no-gutters>
-        <v-col cols="12" sm="4" class="pa-2"> Enjoy your word cards! </v-col>
-        <v-col cols="12" sm="4" class="pa-2">
+      <v-row no-gutters class="d-flex">
+        <v-col
+          id="slogan"
+          cols="12"
+          lg="4"
+          md="6"
+          class="f-section pa-2 font-weight-bold font-italic"
+        >
+          Enjoy your word cards!
+        </v-col>
+        <v-col cols="12" lg="4" md="6" class="f-section pa-2">
           Developed by <a href="https://github.com/ArtuGit">Artu</a>
         </v-col>
-        <v-col cols="12" sm="4" class="pa-2">
+        <v-col cols="12" lg="4" md="12" class="f-section pa-2">
           <div>Powered by</div>
           <div class="logo">
             <a href="https://pixabay.com/" target="_blank"
@@ -26,7 +34,7 @@
         </v-col>
       </v-row>
       <v-row no-gutters>
-        <v-col cols="12" class="pa-2">
+        <v-col cols="12" class="pa-2 text-center">
           {{ new Date().getFullYear() }}
         </v-col>
       </v-row>
@@ -42,8 +50,17 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
 .logo img {
   max-height: 50px;
+}
+.f-section {
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+#slogan {
+  letter-spacing: 2px;
 }
 </style>
