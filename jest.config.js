df8b4e3eb19e08c33fc281ce69e7cff87d181bd0
Artem L <artuon@gmail.com>
2021-03-30 12:00:33 +0300

    Adding Jest Testing (technically)

---
+++ jest.config.js
@@ -0,0 +1,22 @@
+/*
+ * Developed by Artu, https://github.com/ArtuGit
+ *  Copyleft 2020-2021.
+ */
+
+module.exports = {
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/$1',
+    '^~/(.*)$': '<rootDir>/$1',
+    '^vue$': 'vue/dist/vue.common.js',
+  },
+  moduleFileExtensions: ['js', 'vue', 'json'],
+  transform: {
+    '^.+\\.js$': 'babel-jest',
+    '.*\\.(vue)$': 'vue-jest',
+  },
+  collectCoverage: true,
+  collectCoverageFrom: [
+    '<rootDir>/components/**/*.vue',
+    '<rootDir>/pages/**/*.vue',
+  ],
+}
---
+++ test/test.test.js
@@ -0,0 +1,49 @@
+/*
+ * Developed by Artu, https://github.com/ArtuGit
+ *  Copyleft 2020-2021.
+ */
+
+// Import the `mount()` method from Vue Test Utils
+import { mount } from '@vue/test-utils'
+
+// The component to test
+const MessageComponent = {
+  template: '<p>{{ msg }}</p>',
+  props: ['msg']
+}
+
+const Counter = {
+  template: `
+    <div>
+      <button @click="count++">Add up</button>
+      <p>Total clicks: {{ count }}</p>
+    </div>
+  `,
+  data() {
+    return { count: 0 }
+  }
+}
+
+test('increments counter value on click', async () => {
+  const wrapper = mount(Counter)
+  const button = wrapper.find('button')
+  const text = wrapper.find('p')
+
+  expect(text.text()).toContain('Total clicks: 0')
+
+  await button.trigger('click')
+
+  expect(text.text()).toContain('Total clicks: 1')
+})
+
+test('displays message', () => {
+  // mount() returns a wrapped Vue component we can interact with
+  const wrapper = mount(MessageComponent, {
+    propsData: {
+      msg: 'Hello world'
+    }
+  })
+
+  // Assert the rendered text of the component
+  expect(wrapper.text()).toContain('Hello world')
+})
